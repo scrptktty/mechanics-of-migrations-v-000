77478f06674d289563a14073ee6f14628a1c5150
Kachel Wittig <kjwittig@gmail.com>
2018-06-26 16:32:48 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,8 @@
 require 'bundler/setup'
 Bundler.require
 
+require_relative "../artist.rb"
+
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/artists.sqlite"
--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,9 +1,4 @@
 class CreateArtists < ActiveRecord::Migration
-  def up
-  end
-
-  def down
-  end
 
   def change
     create_table :artists do |t|
@@ -13,5 +8,4 @@ class CreateArtists < ActiveRecord::Migration
       t.string :hometown
     end
   end
-
 end # CreateArtists
